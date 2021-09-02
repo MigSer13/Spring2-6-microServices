d487e647fad7b00fcd307adddc98e395e5bea57f
MigSer13 <avto470@mail.ru>
2021-09-02 16:02:39 +0300

    questions

--- clientDB/src/main/java/com/myhomeDB/AppDB.java
+++ clientDB/src/main/java/com/myhomeDB/AppDB.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(scanBasePackages = {"test"} , exclude = JpaRepositoriesAutoConfiguration.class)
-//@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = {"test"} , exclude = JpaRepositoriesAutoConfiguration.class)
+@SpringBootApplication
 @EnableEurekaClient
 public class AppDB {
     public static void main(String[] args) {
