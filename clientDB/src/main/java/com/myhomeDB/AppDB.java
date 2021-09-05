3280326b0cf03d591e9ade5f1b9eb960479d4808
MigSer13 <avto470@mail.ru>
2021-09-05 18:36:24 +0300

    works DB & eureka

--- clientDB/src/main/java/com/myhomeDB/AppDB.java
+++ clientDB/src/main/java/com/myhomeDB/AppDB.java
@@ -2,11 +2,11 @@ package com.myhomeDB;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.cloud.autoconfigure.ConfigurationPropertiesRebinderAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(scanBasePackages = {"test"} , exclude = JpaRepositoriesAutoConfiguration.class)
-//@SpringBootApplication
+//@SpringBootApplication(scanBasePackages = {"test"} , exclude = ConfigurationPropertiesRebinderAutoConfiguration.class)
+@SpringBootApplication
 @EnableEurekaClient
 public class AppDB {
     public static void main(String[] args) {
--- clientFront/src/main/java/com/myhome/ClientFrontApp.java
+++ clientFront/src/main/java/com/myhome/ClientFrontApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(scanBasePackages = {"test"} , exclude = JpaRepositoriesAutoConfiguration.class)
+@SpringBootApplication (scanBasePackages = {"test"} , exclude = JpaRepositoriesAutoConfiguration.class)
 @EnableEurekaClient
 public class ClientFrontApp {
 
